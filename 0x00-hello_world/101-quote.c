8231bbcdef70e03520feb7af020786b565d1233a
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-02 04:31:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
-/**
+#include <stdio.h>                                                                              
+                                                                                                
+/**                                                                                             
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
-int main(void)
+ */                                                                                             
+int main(void)                                                                                  
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	return (1);
+      	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);                                                                                         
+        return (1);                                                                             
 }
