a86819c0322a28ded7e261262ee2ce5702ae6b1e
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-02 02:49:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,9 @@
  */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	char Mystr;
+
+	Mystr = "\"Programming is like building a multilingual puzzle";
 	puts(Mystr);
 
 	return (0);
