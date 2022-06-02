b6b08940d6b449ff168937015f03af4d15a942ea
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-02 02:38:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -std puts.c
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return (0);
+}
