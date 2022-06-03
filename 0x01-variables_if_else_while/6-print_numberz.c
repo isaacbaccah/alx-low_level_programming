2bb795a64b3eb8b854939b7515cb1a867d108589
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-03 06:24:01 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints all single digit numbers of base 10 starting from 0,
- * 	  only using putchar and without char variables.
+ * only using putchar and without char variables.
  *
  * Return: Always 0.
  */
