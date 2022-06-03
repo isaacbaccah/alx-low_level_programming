be22c93626824d0d1fc4818d10da23f1175fdc18
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-03 12:26:16 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <=98; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
@@ -27,7 +27,7 @@ int main(void)
 			putchar(' ');
 		}
 	}
-	
+
 	putchar('\n');
 
 	return (0);
