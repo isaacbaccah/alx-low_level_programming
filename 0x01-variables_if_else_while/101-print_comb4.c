8bd419d317fbdd6d7388b78058e7a4611cb3291e
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-03 12:16:23 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
 
-				if (digit1 == 7 && digit2 ==8 && digit3 == 9)
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 
 				putchar(',');
