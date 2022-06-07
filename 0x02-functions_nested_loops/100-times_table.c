0146f661cc3768a7d81c299790d09acd2fe5b484
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-07 16:43:03 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(32);
 					_putchar((z / 100) + '0');
-					_putchar(((z / 100) % 10) + '0');
+					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
 				else if (z > 9)
