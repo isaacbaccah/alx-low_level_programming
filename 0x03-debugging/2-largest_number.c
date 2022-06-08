76bafdd1e8109a9108cc76ce41eb38057e3a814f
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-07 18:45:37 -0700

    2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_numer - returns the largest of 3 numbers
+ * @: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_nuber(int a, int b, int c)
+{
+	int largest;
+
+	if a(a > b && a > c)
+	{
+		largest = a;
+	} else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
