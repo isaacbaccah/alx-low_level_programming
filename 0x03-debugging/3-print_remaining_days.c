8b585736965cf46ce4349926174d51e81cea8f34
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-08 02:12:37 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -31,7 +31,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
