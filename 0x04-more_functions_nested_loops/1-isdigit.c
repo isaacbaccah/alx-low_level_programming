26864d4ce46aa285c15433dda0dae07a9716f186
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-09 06:04:42 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * _isdigit - a function that checks for a digit 0-9
  * @c: input
- * Return 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
