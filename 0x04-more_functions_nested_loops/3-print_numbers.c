cf2f85edceedac94927573eb78d6c4a3c68a0970
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-09 06:32:44 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9
+ * print_numbers - prints 0123456789\n
  *
- * Return: Always 0.
+ * Return: void
  */
 void print_numbers(void)
 {
@@ -14,6 +14,4 @@ void print_numbers(void)
 		_putchar(c);
 	}
 	_putchar('\n');
-
-	return (0);
 }
