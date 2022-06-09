116669255abb933185ab1c2e7d82f0efced4fa61
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-09 06:03:20 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,8 @@
 /**
  * _isupper - a function that checks for uppercase character
  * @c: input
- * Return 1 if c is uppercase, 0 otherwise
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
