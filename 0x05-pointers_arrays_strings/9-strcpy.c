f35f77f6599004272cb14adc3146d4d3ba2d5030
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-13 01:05:10 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	for (index = 0; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
-		index++;
 	}
+	dest[index] = '\0';
 
 	return (dest);
 }
