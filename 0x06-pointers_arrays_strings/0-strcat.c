100fa79dff5d88c9287094ccfcbe0c42bd097876
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-15 11:14:14 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src,
+ * _strcat - Concatenates the string pointed to by @src,
  * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
