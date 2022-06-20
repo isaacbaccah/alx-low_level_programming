530b120d9b9b13d601f2ea877bb709d223218f16
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-20 05:11:00 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * memset - Fills the first n bytes of the memory area
  * pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
+ * @b: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
  * Return: A pointer to the filled memory area @s.
