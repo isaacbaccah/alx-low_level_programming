c47b68925434ca351d9cad785d215af1eeb4875a
Isaac Baccah <isaacbaccah@icloud.com>
2022-06-20 01:33:36 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,10 +9,10 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = c;
+	char *memory = s, value = b;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
