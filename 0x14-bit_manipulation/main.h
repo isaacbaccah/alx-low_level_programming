43311567e432457a451c7e8e38334066c71b4efd
Isaac Baccah <isaacbaccah@icloud.com>
2022-07-21 10:25:21 -0700

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: holberton.h
- * AUth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
- */
-
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
